9993a2d67f9a0aaef8229f1285a658c7efa21ebb
Daniel St. Clair <daniel.stclair@favordelivery.com>
2020-10-08 19:13:33 -0500

    finish app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,88 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
 
-function App() {
+function Button(props) {
+  const { type = 'button', onClick, children, className = '' } = props
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <button type={type} onClick={onClick} className={className}>
+      {children}
+    </button>
+  )
 }
 
-export default App;
+function Counter(props) {
+  const { step = 1, limit, onIncrease, onDecrease } = props
+
+  const [count, setCount] = useState(0)
+
+  const increaseCount = () => {
+    if (!limit || limit > count) {
+      const newCount = count + step
+      setCount(newCount)
+      onIncrease(step)
+    }
+  }
+
+  const decreaseCount = () => {
+    if (count) {
+      const newCount = count - step
+      setCount(newCount)
+      onDecrease(step)
+    }
+  }
+
+  const resetCount = () => {
+    onDecrease(count)
+    setCount(0)
+  }
+
+  return (
+    <div class="app">
+      <div>
+        <div class="count">
+          <h3>Count:</h3>
+          <h1>{count}</h1>
+        </div>
+        <div class="buttons">
+          <Button onClick={decreaseCount}>-</Button>
+          <Button onClick={increaseCount}>+</Button>
+        </div>
+        <Button onClick={resetCount} className="reset">
+          reset
+        </Button>
+      </div>
+    </div>
+  )
+}
+
+function App() {
+  const [totalCount, setTotalCount] = useState(0)
+
+  const increaseCount = count => {
+    setTotalCount(totalCount + count)
+  }
+
+  const decreaseCount = count => {
+    setTotalCount(totalCount - count)
+  }
+
+  return (
+    <div>
+      <h1>I am the total count: {totalCount}</h1>
+      <Counter
+        step={5}
+        limit={20}
+        onIncrease={increaseCount}
+        onDecrease={decreaseCount}
+      />
+      <Counter step={2} onIncrease={increaseCount} onDecrease={decreaseCount} />
+      <Counter
+        step={9}
+        limit={30}
+        onIncrease={increaseCount}
+        onDecrease={decreaseCount}
+      />
+    </div>
+  )
+}
+
+export default App
